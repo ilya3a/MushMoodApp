7782e3aeda4ee649c6b59877ab5491533fc26c80
ilya aptekar <42389926+ilya3a@users.noreply.github.com>
2025-08-20 15:01:03 +0300

    feat: enable kiosk-lite back block and lock task
    Block back navigation and start lock task after first frame. Declare HOME+DEFAULT intent filter so the app can be set as home.\n\nCloses #10\nRefs #1

--- app/src/main/java/com/yoyo/mushmoodapp/ui/MainScreen.kt
+++ app/src/main/java/com/yoyo/mushmoodapp/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.yoyo.mushmoodapp.ui
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
@@ -7,14 +8,7 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +27,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -46,6 +41,7 @@ import com.yoyo.mushmoodapp.scenes.GradientSpec
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    BackHandler { }
     val state by viewModel.uiState.collectAsState()
 
     val gradientSpec = state.runningSceneId?.let { SceneDefs.scenes[it].gradient } ?: SceneDefs.defaultGradient
